bed75518694c6bd3cbd43b9d0c5f8f01783a79f5
Foster <Tim.Foster@SEPA.org.uk>
2019-09-24 12:53:14 +0100

    test: add test results to compare

--- tests/testthat/test-calcClassScot.R
+++ tests/testthat/test-calcClassScot.R
@@ -1,5 +1,5 @@
 test_that("test results match pre-calculated results", {
   results <- calcClassScot(data = fcs2::demo_data)
 
-  expect_equal(results, demo_results)
+  expect_equal(results,  fcs2:::demo_results)
 })
